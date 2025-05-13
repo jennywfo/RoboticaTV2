a1bc31539050960a317180a85233740406e1d2f5
jenny <114223410+jennywfo@users.noreply.github.com>
2025-05-13 12:17:24 +0200

    he afegit el calibrador per els sensors
    el que dona es: minimos
    36
    76
    76
    36
    36
    36

--- codi_robot/codi_robot.ino
+++ codi_robot/codi_robot.ino
@@ -1,4 +1,8 @@
 #include <QTRSensors.h>
+
+QTRSensors qtr;
+const uint8_t SensorCount =6;
+uint16_t sensorValues[SensorCount];
 int E1 = 6;
 int M1 = 7;
 int E2 = 5;
@@ -16,12 +20,45 @@ void blink(){
 }
 void setup()
 {
+   
     pinMode(M1, OUTPUT);
     pinMode(M2, OUTPUT);
     pinMode(LED_BUILTIN, OUTPUT);
+    // configurar sensors
+    qtr.setTypeRC();
+    qtr.setSensorPins((const uint8_t[]){1,2,3,4,6,7},SensorCount);
+
+    // 2.5 ms RC read timeout (default)* 10 reads per calibrate() call
+    // = mas o menos 25 ms per calibrate() call.
+    // call calibrate() 400 times to make calibration take about 20 seconds
+    for(uint16_t i =0; i <400;i++){
+      qtr.calibrate();
+    }
+    Serial.begin(9600);
+  
+    Serial.println("minimos");
+    for(uint8_t i = 0; i < SensorCount;i++){
+       
+         Serial.println(qtr.calibrationOn.minimum[i]);
+       
+    }
+
+    Serial.println();
+    Serial.println("maximos");
+   for(uint8_t i = 0; i < SensorCount;i++){
+
+         Serial.println(qtr.calibrationOn.maximum[i]);
+       
+    }
+    Serial.println();
+    Serial.println();
+    delay(1000);
+
+
 }
 void loop() {
 
-   avanzar();
+
+
 
 }
