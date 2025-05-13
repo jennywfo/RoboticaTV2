e31040d7b929467996e4865961e36a6b2651cd66
jenny <114223410+jennywfo@users.noreply.github.com>
2025-05-13 15:47:06 +0200

    Update codi_robot.ino

--- codi_robot/codi_robot.ino
+++ codi_robot/codi_robot.ino
@@ -7,9 +7,9 @@ int E1 = 6;
 int M1 = 7; // dreta
 int E2 = 5;
 int M2 = 4; // esquerra
-
-int vel_esq = 200;
-int vel_dret = 200;
+int vel_base = 180;
+int vel_esq = 0;
+int vel_dret = 0;
 void avanzar(){
   analogWrite(E1, vel_dret); // difèrencia de 5 
   analogWrite(E2, vel_esq);
@@ -30,7 +30,7 @@ void setup()
 
     // configurar sensors
     qtr.setTypeRC();
-    qtr.setSensorPins((const uint8_t[]){1,2,3,4,6,7},SensorCount);
+    qtr.setSensorPins((const uint8_t[]){9,8,3,2,1,10},SensorCount);
 
     // 2.5 ms RC read timeout (default)* 10 reads per calibrate() call
     // = mas o menos 25 ms per calibrate() call.
@@ -59,30 +59,21 @@ void setup()
     
     delay(1000);
 
-    avanzar();
-    delay(1000);
-
 
 }
 void loop() {
   
   uint16_t posicion = qtr.readLineBlack(sensorValues);
-
+  // Calcula el error
+  int error = 2500 - posicion;
+  int vel_motor = error * 0.2;
+  vel_dret = vel_base - vel_motor;
+  vel_esq = vel_base + vel_motor;
   Serial.println(posicion);
-  delay(250);
-
-  if(posicion > 600 && vel_esq != 250){
-    vel_esq += 10;
-    vel_dret = 100;
-  }
+  analogWrite(E1, constrain(vel_dret,0,255)); // difèrencia de 5 
+  analogWrite(E2, constrain(vel_esq,0,255));
+  delay(2);
   
-  if(posicion < 600 && vel_dret != 250){
-    vel_dret += 10;
-    vel_esq = 200;
-  }
-  analogWrite(E1, vel_dret); // difèrencia de 5 
-  analogWrite(E2, vel_esq);
-
 
 
 
