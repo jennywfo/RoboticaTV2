f58ad4ff5289c2641ed307220757a88ee905a94e
jenny <114223410+jennywfo@users.noreply.github.com>
2025-05-13 11:34:22 +0200

    funcion de blink y avanzar

--- codi_robot/codi_robot.ino
+++ codi_robot/codi_robot.ino
@@ -1,10 +1,27 @@
-void setup() {
-  // put your setup code here, to run once:
-  
-
+#include <QTRSensors.h>
+int E1 = 6;
+int M1 = 7;
+int E2 = 5;
+int M2 = 4;
+void avanzar(){
+  analogWrite(E1, 255); // difèrencia de 5 
+  analogWrite(E2, 255);
 }
 
+void blink(){
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(1000);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(1000);
+}
+void setup()
+{
+    pinMode(M1, OUTPUT);
+    pinMode(M2, OUTPUT);
+    pinMode(LED_BUILTIN, OUTPUT);
+}
 void loop() {
-  // put your main code here, to run repeatedly:
+
+   avanzar();
 
 }
